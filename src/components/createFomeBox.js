64850b8acae3844af3b53a0e51fdf4f5fd90ffc8
anurag-shukla8004 <79192104+anurag-shukla8004@users.noreply.github.com>
2022-01-07 15:13:15 +0530

    Add files via upload

--- src/components/createFomeBox.js
+++ src/components/createFomeBox.js
@@ -1,6 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CreateFomeBox() {
+  const [startDate, setStartDate] = useState();
+  const [startTime, setStartTime] = useState();
+  const [endDate, setEndDate] = useState();
+  const [endTime, setEndTime] = useState();
+  const [maxEndDate, setMaxDate] = useState();
+  const [minEndDate, setMinDate] = useState();
+
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+
+  today = yyyy + '-' + mm + '-' + dd;
+  console.log(today, ' hekadjfakdj');
+
+  Date.prototype.addDays = function (days, value) {
+    const date = new Date(value);
+    date.setDate(date.getDate() + days);
+    return date;
+  };
+
+  function convert(str) {
+    var date = new Date(str),
+      mnth = ('0' + (date.getMonth() + 1)).slice(-2),
+      day = ('0' + date.getDate()).slice(-2);
+    return [date.getFullYear(), mnth, day].join('-');
+  }
+
+  const addDaysInStartDate = (value) => {
+    let date = new Date();
+    return date.addDays(7, value);
+  };
+
+  const dateConverter = (date) => {
+    date = convert(date);
+    return date;
+  };
+
+  const updateStartDate = (e) => {
+    let date = addDaysInStartDate(e.target.value);
+    console.log(date);
+    date = dateConverter(date);
+    setStartDate(e.target.value);
+    console.log(date);
+    setEndDate(date);
+    setMaxDate(date);
+    setMinDate(e.target.value);
+  };
+  const updateEndDate = (e) => {
+    console.log(e.target.value);
+    setEndDate(e.target.value);
+  };
   return (
     <>
       <div className="createFomeBox-container">
@@ -16,21 +68,34 @@ function CreateFomeBox() {
           <div className="eventdate-start">
             <div className="eventdate-start-date event">
               <h4>Start Date</h4>
-              <input type="date" id="start-date" />
+              <input
+                type="date"
+                id="start-date"
+                onChange={updateStartDate}
+                value={startDate}
+                min={today}
+              />
             </div>
             <div className="eventdate-start-time event">
               <h4>Start Time</h4>
-              <input type="time" id="start-time"></input>
+              <input type="time" id="start-time" value={startTime}></input>
             </div>
           </div>
           <div className="eventdate-ends">
             <div className="eventdate-end-date event">
               <h4>End Date</h4>
-              <input type="date" id="start-date" />
+              <input
+                type="date"
+                id="end-date"
+                onChange={updateEndDate}
+                max={maxEndDate}
+                min={minEndDate}
+                value={endDate}
+              />
             </div>
             <div className="eventdate-end-time event">
               <h4>End Time</h4>
-              <input type="time" id="start-time"></input>
+              <input type="time" id="end-time" value={endTime}></input>
             </div>
           </div>
         </div>
--- src/pages/create.js
+++ src/pages/create.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CreateNavBar from '../components/createNavBar';
 import CreateFomeBox from '../components/createFomeBox';
+import Header from '../components/header';
 
 const CreatePage = () => {
   return (
     <>
       <div className="create-container">
+        <Header />
         <CreateNavBar />
         <CreateFomeBox />
       </div>
